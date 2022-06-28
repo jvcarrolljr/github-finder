64b1cdb9fa7cc51e94386137cdeb70719931e23e
Joseph Valle <jvcarrolljr@gmail.com>
2022-06-28 18:02:39 -0400

    New Change

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,6 +5,8 @@ function Home() {
     <>
       {/* SEARCH COMPONENT */}
       <UserResults />
+      {/* REACT_APP_GITHUB_URL="https://api.github.com"
+REACT_APP_GITHUB_TOKEN="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" */}
     </>
   )
 }
