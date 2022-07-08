6b299be99e6eed077595e2bb66186f19ff58fc76
Joseph Valle <jvcarrolljr@gmail.com>
2022-07-07 23:02:27 -0400

    to non-remote

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,10 +3,8 @@ import UserResults from "../components/users/UserResults"
 function Home() {
   return (
     <>
-      {/* SEARCH COMPONENT */}
+      {/* Search Component */}
       <UserResults />
-      {/* REACT_APP_GITHUB_URL="https://api.github.com"
-REACT_APP_GITHUB_TOKEN="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" */}
     </>
   )
 }
